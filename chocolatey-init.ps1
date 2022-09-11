860d355c32d3ede07da5ae869fa0882b7b69dae7
Félix Díez <felixeu31@gmail.com>
2022-09-11 11:11:00 +0200

    chocolatey install

---
+++ chocolatey-init.ps1
@@ -0,0 +1,9 @@
+# if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false -or [string]::IsNullOrWhiteSpace($dismAppList) -eq $false)
+# {
+    try{
+        choco config get cacheLocation
+    }catch{
+        Write-Output "Chocolatey not detected, trying to install now"
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+# }
